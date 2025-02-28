fc4d9690b9392bbf12e459d7f570fad8873f22f6
Saulo Ferreira de Souza <saulosouza@proton.me>
2025-02-27 22:37:09 -0300

    Implemented creation process for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,32 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show ]
   def index
     @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,9 +7,26 @@ describe "navigate" do
       expect(page.status_code).to eq 200
     end
 
-    it "has a list of posts" do
+    it "has a title of Posts" do
       visit posts_path
       expect(page).to have_content "Posts"
+    end
+  end
+
+  describe "creation" do
+    before do
+      visit new_post_path
+    end
+
+    it "has a new form that can be reached" do
+      expect(page.status_code).to eq 200
+    end
+
+    it "can be created from new form page" do
+      fill_in "post[date]", with: "2025-02-28"
+      fill_in "post[rationale]", with: "Some rationale"
+      click_button "Save"
+      expect(page).to have_content "Some rationale"
     end
   end
 end

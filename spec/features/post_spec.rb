70209ac5c44d05e745c42c44a5b54edf34891638
Saulo Ferreira de Souza <saulosouza@proton.me>
2025-02-27 22:10:37 -0300

    Created index action for posts

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "navigate" do
+  describe "to the home page" do
+    it "can be reached successfully" do
+      visit posts_path
+      expect(page.status_code).to eq 200
+    end
+
+    it "has a list of posts" do
+      visit posts_path
+      expect(page).to have_content "Posts"
+    end
+  end
+end

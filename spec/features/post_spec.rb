a430782bbe3228e67e1fba83b0de76b79bd82684
Saulo Ferreira de Souza <saulosouza@proton.me>
2025-02-27 22:19:24 -0300

    Fix test description

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "navigate" do
-  describe "to the home page" do
+  describe "index" do
     it "can be reached successfully" do
       visit posts_path
       expect(page.status_code).to eq 200

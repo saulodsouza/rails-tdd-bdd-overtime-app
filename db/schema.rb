018af237d1c49df3aa7226af2546fa98197eb2e5
Saulo Ferreira de Souza <saulosouza@proton.me>
2025-02-27 21:21:29 -0300

    Add devise trackable on user

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_28_001207) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.string "type"

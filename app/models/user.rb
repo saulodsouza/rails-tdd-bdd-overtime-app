7b1ee14041ee1082c86e4c8c8dc14680c2c479e9
Saulo Ferreira de Souza <saulosouza@proton.me>
2025-02-27 21:38:32 -0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates_presence_of :first_name, :last_name
 end
